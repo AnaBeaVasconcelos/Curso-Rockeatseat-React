1fdaad9b0e982cfa876b138755080ab6b1199f9f
AnaBeaVasconcelos <bihhtuubee@gmail.com>
2022-11-27 11:41:39 -0300

    configuranda o projeto em react

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import { render } from "react-dom"
+import { App } from './App'
+
+render(<App />, document.getElementById('root'))
+
+// Quando houver html dentro do javascript, é necessário renomear o arquivo para JSX.
+// JSX é uma extensão do javascript que permite escrever html dentro do javascript.
